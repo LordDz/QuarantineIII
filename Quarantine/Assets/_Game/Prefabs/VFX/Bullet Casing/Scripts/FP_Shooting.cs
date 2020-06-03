081f85ddbd395e552fd9c44ff32a355e6b674385
David Zetterdahl <david_zetterdahl@hotmail.com>
2020-06-03 20:59:28 +0200

    BEER BEER BEER BEER and a few props.

--- Quarantine/Assets/_Game/Prefabs/VFX/Bullet Casing/Scripts/FP_Shooting.cs
+++ Quarantine/Assets/_Game/Prefabs/VFX/Bullet Casing/Scripts/FP_Shooting.cs
@@ -3,8 +3,13 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class FP_Shooting : MonoBehaviour {
-	
+
     public GameObject Cardridge;
+
+
+    private void test()
+    {
+    }
 
     public void Shoot()
     {
@@ -13,6 +18,17 @@ public class FP_Shooting : MonoBehaviour {
         Physics.IgnoreCollision(playerCollider, shell.GetComponentInChildren<Collider>());
         shell.GetComponentInChildren<Rigidbody>().AddForce(50 * Vector3.up + (50 * -Vector3.forward));
         Destroy(shell, 15f);
+    }
+
+    public double Shoot(int time, double test, double test2)
+    {
+        Collider playerCollider = FindObjectOfType<PlayerCharacterController>().GetComponent<Collider>();
+        var shell = Instantiate(Cardridge, transform.position, Quaternion.identity);
+        Physics.IgnoreCollision(playerCollider, shell.GetComponentInChildren<Collider>());
+        shell.GetComponentInChildren<Rigidbody>().AddForce(50 * Vector3.up + (50 * -Vector3.forward));
+        Destroy(shell, time);
+
+        return 1;
     }
 }
 
